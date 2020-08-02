b6387dd30f6515c57c179ce7575fe8075b72e364
Bjarne <bjarne_priebe@hotmail.com>
2020-08-02 19:24:35 +0200

    skip two in array, site dump

--- scrapetable.py
+++ scrapetable.py
@@ -12,18 +12,19 @@ data = {}
 # https://www.codementor.io/@dankhan/web-scrapping-using-python-and-beautifulsoup-o3hxadit4
 # http://tset.ninja/2018/12/02/2018-11-15-handuo-six-stars-movie-prediction/
 
-# test = tb.find_all('tr')
-# next(test)
-counter = 0
+counter = -1
 
-for link in tb.find_all('tr'):
-    if counter == 30:
+all_tr = tb.find_all('tr');
+# print(t.counter);
+
+for link in all_tr[2:10]:
+    counter += 1
+
+    print(link);
+    # for l in link.find_all('td'):
+    if counter == 10:
         break
 
-    name = link.find('td')
-    print(name)
-
-    counter += 1
-    # break;
-    # print(name.get_text('href'))
+        # name = link.find('td')
+        # print(name.get_text('href'))
 
